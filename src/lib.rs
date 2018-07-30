443d742e0c96308cc21e2387c8f0683779a91b94
Axel Forsman <axelsfor@gmail.com>
2018-07-30 22:46:25 +0200

    Preallocate memory for results array.
    This results in performance improvements.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,11 @@
 /// Returns the indices of the longest increasing subsequence in `a`.
 ///
+/// This is O(n log n).
+///
+/// # Panics
+///
+/// Panics if `a.len()` is zero.
+///
 /// # Example
 ///
 /// ```
@@ -7,50 +13,56 @@
 /// assert_eq!(longest_increasing_subsequence(&[2, 1, 4, 3, 5]), [1, 3, 4]);
 /// ```
 pub fn longest_increasing_subsequence<T: PartialOrd>(a: &[T]) -> Vec<usize> {
-    let mut p = vec![0; a.len()];
-    let mut result = vec![0];
-
+    let (mut p, mut m) = (vec![0; a.len()], vec![0; a.len()]);
+    let mut l = 1;
     for i in 1..a.len() {
-        let k = &a[i];
-
-        // Test whether k can be used to extend current sequence
-        let j = *result.last().unwrap();
-        if &a[j] < k {
+        // Test whether a[i] can extend the current sequence
+        let j = m[l - 1];
+        if a[j] < a[i] {
             p[i] = j;
-            result.push(i);
+            m[l] = i;
+            l += 1;
             continue;
         }
 
         // Binary search
-        let (mut lo, mut hi) = (0, result.len() - 1);
+        let (mut lo, mut hi) = (0, l - 1);
         while lo < hi {
             let mid = (lo + hi) / 2;
-            if &a[result[mid]] < k {
+            if a[m[mid]] < a[i] {
                 lo = mid + 1;
             } else {
                 hi = mid;
             }
         }
 
-        if k < &a[result[lo]] {
-            if lo > 0 {
-                p[i] = result[lo - 1];
-            }
-            result[lo] = i;
+        if lo > 0 {
+            p[i] = m[lo - 1];
         }
+        m[lo] = i;
     }
 
-    let mut v = *result.last().unwrap();
-    for i in (0..result.len()).rev() {
-        result[i] = v;
-        v = p[v];
+    // Reconstruct the longest increasing subsequence
+    let mut k = m[l - 1];
+    unsafe {
+        m.set_len(l);
     }
-    result
+    for i in (0..l).rev() {
+        m[i] = k;
+        k = p[k];
+    }
+    m
 }
 
 #[cfg(test)]
 mod tests {
-    use longest_increasing_subsequence;
+    use super::*;
+
+    #[test]
+    #[should_panic]
+    fn len_zero() {
+        longest_increasing_subsequence::<i32>(&[]);
+    }
 
     #[test]
     fn len_one() {
@@ -60,7 +72,13 @@ mod tests {
 
     #[test]
     fn lis_test() {
-        assert_eq!(longest_increasing_subsequence(&[2, 1, 4, 3, 5]), [1, 3, 4]);
-        assert_eq!(longest_increasing_subsequence(&[2, 3, 4, 3, 5]), [0, 1, 2, 4]);
+        assert_eq!(
+            longest_increasing_subsequence(&[0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15]),
+            [0, 4, 6, 9, 13, 15]
+        );
+        assert_eq!(
+            longest_increasing_subsequence(&[2, 3, 4, 3, 5]),
+            [0, 1, 2, 4]
+        );
     }
 }
