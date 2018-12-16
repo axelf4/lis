ee0ac53a399ceb703ff85a2ddaf1ade9d36d24c5
Axel Forsman <axelsfor@gmail.com>
2018-12-16 12:51:38 +0100

    Add peekable double ended iterator

--- src/lib.rs
+++ src/lib.rs
@@ -74,6 +74,110 @@ pub trait LisExt<T>: AsRef<[T]> {
 
 impl<S, T> LisExt<T> for S where S: AsRef<[T]> + ?Sized {}
 
+/// Extends `Iterator` with an adapter that is peekable from both ends.
+trait IteratorExt: Iterator {
+    fn de_peekable(self) -> DePeekable<Self>
+    where
+        Self: Sized + DoubleEndedIterator,
+    {
+        DePeekable {
+            iter: self,
+            front: None,
+            back: None,
+        }
+    }
+}
+
+impl<T: Iterator> IteratorExt for T {}
+
+/// A double ended iterator that is peekable.
+#[derive(Clone, Debug)]
+#[must_use = "iterator adaptors are lazy and do nothing unless consumed"]
+struct DePeekable<I: Iterator> {
+    iter: I,
+    front: Option<Option<I::Item>>,
+    back: Option<Option<I::Item>>,
+}
+
+impl<I: Iterator> DePeekable<I> {
+    #[inline]
+    fn peek(&mut self) -> Option<&I::Item> {
+        if self.front.is_none() {
+            self.front = Some(
+                self.iter
+                    .next()
+                    .or_else(|| self.back.take().unwrap_or(None)),
+            );
+        }
+        match self.front {
+            Some(Some(ref value)) => Some(value),
+            Some(None) => None,
+            _ => unreachable!(),
+        }
+    }
+
+    #[inline]
+    fn peek_back(&mut self) -> Option<&I::Item>
+    where
+        I: DoubleEndedIterator,
+    {
+        if self.back.is_none() {
+            self.back = Some(
+                self.iter
+                    .next_back()
+                    .or_else(|| self.front.take().unwrap_or(None)),
+            );
+        }
+        match self.back {
+            Some(Some(ref value)) => Some(value),
+            Some(None) => None,
+            _ => unreachable!(),
+        }
+    }
+}
+
+impl<I: Iterator> Iterator for DePeekable<I> {
+    type Item = I::Item;
+    #[inline]
+    fn next(&mut self) -> Option<I::Item> {
+        self.front
+            .take()
+            .unwrap_or_else(|| self.iter.next())
+            .or_else(|| self.back.take().unwrap_or(None))
+    }
+
+    #[inline]
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        let peek_len = match self.front {
+            Some(None) => return (0, Some(0)),
+            Some(Some(_)) => 1,
+            None => 0,
+        } + match self.back {
+            Some(None) => return (0, Some(0)),
+            Some(Some(_)) => 1,
+            None => 0,
+        };
+        let (lo, hi) = self.iter.size_hint();
+        (
+            lo.saturating_add(peek_len),
+            hi.and_then(|x| x.checked_add(peek_len)),
+        )
+    }
+}
+
+impl<I: DoubleEndedIterator> DoubleEndedIterator for DePeekable<I> {
+    #[inline]
+    fn next_back(&mut self) -> Option<Self::Item> {
+        self.back
+            .take()
+            .unwrap_or_else(|| self.iter.next_back())
+            .or_else(|| self.front.take().unwrap_or(None))
+    }
+}
+
+impl<I: std::iter::ExactSizeIterator> std::iter::ExactSizeIterator for DePeekable<I> {}
+impl<I: std::iter::FusedIterator> std::iter::FusedIterator for DePeekable<I> {}
+
 #[cfg(test)]
 mod tests {
     use super::*;
